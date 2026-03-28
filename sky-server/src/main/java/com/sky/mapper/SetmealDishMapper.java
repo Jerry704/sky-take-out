e707ba0440051fa9c9ba37147d9e048f41bf5a84
Jerry704 <jerry_7121@hotmail.com>
2026-03-28 21:58:50 +0800

    刪除套餐代碼 未測試

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.SetmealDish;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -26,5 +27,6 @@ public interface SetmealDishMapper {
      * 靠id刪除套餐和菜品的關聯
      * @param setmealId
      */
+    @Delete("delete from setmeal_dish where setmeal_id = #{setmealId}")
     void deleteBySetmealId(Long setmealId);
 }
