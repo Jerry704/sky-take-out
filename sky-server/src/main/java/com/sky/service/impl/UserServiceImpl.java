d2e3c91f4b68c66684a92df54f3f0ffb8d2b0af6
Jerry704 <jerry_7121@hotmail.com>
2026-04-04 02:57:00 +0800

    微信用戶登錄 代碼完成 未測試

---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,83 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    //為信接口地址
+    public static final  String WX_LOGIN ="https://api.weixin.qq.com/sns/jscode2session";
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 調用微信接口，獲得用戶openId
+     * @param code
+     * @return
+     */
+    private String getOpenid(String code){
+
+        Map<String, String> map =new HashMap<>();
+        map.put("appid", weChatProperties.getAppid());
+        map.put("secret", weChatProperties.getSecret());
+        map.put("js_code", code);
+        map.put("grant_type","authorization_code");
+        String json = HttpClientUtil.doGet(WX_LOGIN, map);
+
+        JSONObject jsonObject = JSON.parseObject(json);
+        String openid = jsonObject.getString("openid");
+        return openid;
+    }
+
+    /**
+     * 微信登綠
+     * @param userLoginDTO
+     * @return
+     */
+    @Override
+    public User wxLogin(UserLoginDTO userLoginDTO) {
+        String openid = getOpenid(userLoginDTO.getCode());
+
+        //判斷openid為空，空則拋異常
+        if (openid ==null){
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+
+        //判斷用戶是新用戶(查用戶表)
+        User user = userMapper.getByOpenid(openid);
+
+        //新用戶，自動完成註冊
+        if (user == null){
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+        //返回用戶對象
+        return user;
+    }
+
+
+}
