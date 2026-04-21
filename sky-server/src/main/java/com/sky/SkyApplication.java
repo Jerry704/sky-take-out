a37c7e20859213516ef019054317b9f4aa2d0d7a
Jerry704 <jerry_7121@hotmail.com>
2026-04-22 04:22:46 +0800

    緩存套餐 代碼完成 測試完成

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -3,11 +3,13 @@ package com.sky;
 import lombok.extern.slf4j.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement //开启注解方式的事务管理
+@EnableTransactionManagement //開啟註解方式的事務管理
 @Slf4j
+@EnableCaching //開發緩存註解功能
 public class SkyApplication {
     public static void main(String[] args) {
         //前端需要打開nginx.exe才連得上
